7ed1a0398cb1c046cc71b74db8ae7be2a777c9a8
Raevyn <RaevynWilburn@outlook.com>
2022-06-05 17:58:47 -0700

    added packages

---
+++ src/main/java/com/revature/foundations/models/AppUser.java
@@ -0,0 +1,53 @@
+package com.revature.foundations.models;
+
+import java.util.Objects;
+
+public class AppUser {
+
+    private int id;
+    private String password;
+
+    public AppUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return Objects.equals(username, appUser.username) && Objects.equals(password, appUser.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+
+}
